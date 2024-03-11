35154e44c9017e24199a455c08d557859e44f447
Diego Belli <128156867+diego-belli7@users.noreply.github.com>
2024-03-11 09:03:53 -0300

    Add files via upload

---
+++ carteira.js
@@ -0,0 +1,7 @@
+let idade = 25;
+let possuicarteirademotorista = true;
+
+    if (idade>= 18 && possuicarteirademotorista) {console.log ("pode dirigir");
+ } else {
+    console.log ("não pode dirigir");
+ }
